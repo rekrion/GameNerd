e94d099854b414a32d41c28345bd2e44610016f8
Вадим Джабраилов <vadwave@yandex.ru>
2020-05-26 16:31:44 +0300

    Try Used 2

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance = null;
-    public float sas;
     public List<Category> categories;
 
     void Start()
