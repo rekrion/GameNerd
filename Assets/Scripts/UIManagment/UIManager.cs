9d0308912721ca5c93d5b997e676141c9872b838
Владислав Билецкий <42140267+Aran1ir@users.noreply.github.com>
2020-12-11 20:03:48 +0300

    visual

--- Assets/Scripts/UIManagment/UIManager.cs
+++ Assets/Scripts/UIManagment/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,19 +10,19 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject PlayButton;
     [SerializeField] GameObject SettingButton;
     [SerializeField] GameObject ShopButton;
-
-
-
+    [SerializeField] GameObject PanelShop;
+    [SerializeField] GameObject Background;
+    [SerializeField] GameObject PanelMenu;
+    [SerializeField] GameObject CloseButton;
+    Animator anim;
 
     public void SettingOpen()
     {
-        
 
         if (PanelSetting != null)
 
         {
             Animator animator = PanelSetting.GetComponent<Animator>();
-
             if (animator != null)
             {
                 bool isOpenSetting = animator.GetBool("openSetting");
@@ -31,5 +32,24 @@ public class UIManager : MonoBehaviour
         }
 
     }
-}
 
+    public void ShopOpen()
+    {
+        if (PanelShop != null)
+
+        {
+            Animator animator = PanelShop.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpenShop = animator.GetBool("openShop");
+                animator.SetBool("openShop", !isOpenShop);
+            }
+
+        }
+    }
+
+    public void GameScene (int _sceneNumber)
+    {
+        SceneManager.LoadScene(_sceneNumber);
+    }
+}
