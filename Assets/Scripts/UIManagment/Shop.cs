2a3b436aa8ef849f90618f10f5294240cd24af7f
Владислав Билецкий <42140267+Aran1ir@users.noreply.github.com>
2020-12-21 01:14:29 +0300

    Shop Scene fix

--- Assets/Scripts/UIManagment/Shop.cs
+++ Assets/Scripts/UIManagment/Shop.cs
@@ -27,16 +27,16 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ItemTemplate = ShopScrollView.GetChild(0).gameObject;
+       ItemTemplate = ShopScrollView.GetChild(0).gameObject;
         int len = ShopItemsList.Count;  
-        for (int i = 0; i < 6; i++)   
+        for (int i = 0; i < len; i++)   
         {
             g = Instantiate(ItemTemplate, ShopScrollView);
             g.transform.GetChild(0).GetComponent<Image>().sprite = ShopItemsList[i].Image;
             //g.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = ShopItemsList[i].Price.ToString();
             //g.transform.GetChild(2).GetComponent<Button>().interactable = ShopItemsList[i].IsPurchased;
         }
-        Destroy(ItemTemplate);
+       Destroy(ItemTemplate);
     }
      
     // Update is called once per frame
